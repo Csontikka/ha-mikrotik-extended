a0b5108d6ddea581698b28955c01dd682d28250a
Csontikka <csontikka@hotmail.com>
2026-04-03 21:04:57 +0200

    Replace uptime regex parsing with character loop to eliminate SonarCloud hotspots

--- custom_components/mikrotik_router/coordinator.py
+++ custom_components/mikrotik_router/coordinator.py
@@ -1795,21 +1795,14 @@ class MikrotikCoordinator(DataUpdateCoordinator[None]):
         )
 
         tmp_uptime = 0
-        tmp = re.split(r"(\d++)s", self.ds["resource"]["uptime_str"])
-        if len(tmp) > 1:
-            tmp_uptime += int(tmp[1])
-        tmp = re.split(r"(\d++)m", self.ds["resource"]["uptime_str"])
-        if len(tmp) > 1:
-            tmp_uptime += int(tmp[1]) * 60
-        tmp = re.split(r"(\d++)h", self.ds["resource"]["uptime_str"])
-        if len(tmp) > 1:
-            tmp_uptime += int(tmp[1]) * 3600
-        tmp = re.split(r"(\d++)d", self.ds["resource"]["uptime_str"])
-        if len(tmp) > 1:
-            tmp_uptime += int(tmp[1]) * 86400
-        tmp = re.split(r"(\d++)w", self.ds["resource"]["uptime_str"])
-        if len(tmp) > 1:
-            tmp_uptime += int(tmp[1]) * 604800
+        _uptime_multipliers = {"w": 604800, "d": 86400, "h": 3600, "m": 60, "s": 1}
+        _current = ""
+        for _char in self.ds["resource"]["uptime_str"]:
+            if _char.isdigit():
+                _current += _char
+            elif _char in _uptime_multipliers and _current:
+                tmp_uptime += int(_current) * _uptime_multipliers[_char]
+                _current = ""
 
         self.ds["resource"]["uptime_epoch"] = tmp_uptime
         now = datetime.now().replace(microsecond=0)
